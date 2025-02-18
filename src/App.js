55816977f26269d5a8fcc3744bac351d98952c22
emmanuel <entibimenya03@gmail.com>
2025-02-18 12:18:33 +0000

    profile card -v2: rendering skillsList using map method and rendering level using the && operator instead of ternary operator because we have more than 2 options

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -22,21 +55,25 @@ function Intro() {
     </div>
   );
 }
-function SkillList() {
+function SkillList({ skill, color, level }) {
   return (
     <div className="skill-list">
-      <Skill skill="HTML+CSS" emoji="💪" color="blue" />
-      <Skill skill="JavaScript" emoji="💪" color="orange" />
-      <Skill skill="React" emoji="😎" color="yellow" />
-      <Skill skill="Svelte" emoji="😳" color="orangered" />
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 }
-function Skill(props) {
+function Skill({ skill, level, color }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span> {props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {/*we can not use a ternary operator because we have more than two options so use && operator*/}
+        {level === "beginner" && "😒"}
+        {level === "intermediate" && "😒"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
